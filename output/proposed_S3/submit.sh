3264c7d4268aa790eb00485c81535cf78d2a13b4
jingoystat <54864917+jingoystat@users.noreply.github.com>
2025-05-19 20:32:16 +0800

    add proposed results

---
+++ output/proposed_S3/submit.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+   for s in 80 81 82 83 84 85 86 87 88 89
+    do
+    for r in 2
+do
+for n in 10000 20000
+do
+for J in 30
+do
+for p in 20
+do
+
+    sbatch --export arg1=$s,arg2=$r,arg3=$n,arg4=$J,arg5=$p -o /dev/null -e /dev/null submit.cmd
+
+done
+done
+done
+done
+done
+
+
